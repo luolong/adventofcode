16521f2753b407b0d3f345e5e1c30058e86bd783
Roland Tepp <luolong@gmail.com>
2020-07-11 22:32:08 +0300

    Rust part 2 of Day 1

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,49 +1,53 @@
-use std::fmt::Debug;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-#[derive(Debug)]
-struct Module {
-    mass: i64
+/// # Calculate fuel required for the for the mass.
+///
+/// Fuel required to launch a given module is based on its mass.
+/// Specifically, to find the fuel required for a module,
+/// take its mass, divide by three, round down, and subtract 2.
+fn get_fuel_for_mass(mass: i64) -> i64 {
+  (mass / 3) - 2
 }
 
-impl Module {
-    
-    fn new(mass: i64) -> Module {
-        Module { mass: mass }
-    }
-    
-    fn from_line(input: std::result::Result<std::string::String, std::io::Error>) -> Module {
-      let mass = input.unwrap().parse::<i64>().unwrap();
-      Module { mass: mass }
-    }
+/// # Calculate total fuel required for the module
+///
+/// So, for each module mass, calculate its fuel and add it to the total. 
+/// Then, treat the fuel amount you just calculated as the input mass and repeat the process, 
+/// continuing until a fuel requirement is zero or negative.
+fn get_fuel_for_module(module_mass: i64) -> i64 {
+  let mut total_fuel: i64 = 0;
+  let fuel_for_module = get_fuel_for_mass(module_mass);
+  for fuel_mass in additional_fuel(fuel_for_module) {
+    total_fuel += fuel_mass
+  }
+  total_fuel
+}
 
-    /// # Calculate fuel required for the module.
-    ///
-    /// Fuel required to launch a given module is based on its mass.
-    /// Specifically, to find the fuel required for a module,
-    /// take its mass, divide by three, round down, and subtract 2.
-    ///
-    /// # Example
-    /// ```
-    /// assert_eq!(Module::new(12).get_fuel_required(), 2);
-    /// assert_eq!(Module::new(14).get_fuel_required(), 2);
-    /// assert_eq!(Module::new(1969).get_fuel_required(), 654);
-    /// assert_eq!(Module::new(100756).get_fuel_required(), 33583);
-    /// ```
-
-    fn get_fuel_required(&self) -> i64 {
-        (&self.mass / 3) - 2
+fn additional_fuel(mass: i64) -> impl std::iter::Iterator<Item = i64> {
+  let mut fuel_mass = mass;
+  std::iter::from_fn(move || {
+    let result;
+    if fuel_mass > 0 {
+      result = Some(fuel_mass);
+      fuel_mass = get_fuel_for_mass(fuel_mass);
+    } else {
+      result = None;
     }
+    result
+  })
 }
 
 fn day1() {
   if let Ok(lines) = read_lines("./day1.txt") {
-    let total_mass = lines.map(Module::from_line)
-         .map(|module| { module.get_fuel_required() })
-         .fold(0, |total, mass| { total + mass });
-     println!("Total mass of all modules is {}", total_mass);
+    let mut total_fuel: i64 =  0;
+    for line in lines {
+      let module_mass = line.unwrap().parse().unwrap();
+      total_fuel += get_fuel_for_module(module_mass);
+    }
+
+    println!("Total fuel: {}", total_fuel);
   }
 }
 
@@ -57,4 +61,24 @@ where P: AsRef<Path>, {
 
 fn main() {
     day1()
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+      
+  #[test]
+  fn it_calculates_fuel_for_mass() {
+    assert_eq!(get_fuel_for_mass(12), 2);
+    assert_eq!(get_fuel_for_mass(14), 2);
+    assert_eq!(get_fuel_for_mass(1969), 654);
+    assert_eq!(get_fuel_for_mass(100756), 33583);
+  }
+  
+  #[test]
+  fn it_calculates_fuel_for_module() {
+    assert_eq!(get_fuel_for_module(14), 2);
+    assert_eq!(get_fuel_for_module(1969), 966);
+    assert_eq!(get_fuel_for_module(100756), 50346);
+  }
 }
