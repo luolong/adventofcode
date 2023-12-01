9cadf85cd5a174d6d5a42a0b0622b2ab686831f0
Roland Tepp <roland@gennet.ee>
2023-12-01 21:49:22 +0200

    Solution to day1, part2

--- crates/day1/src/main.rs
+++ crates/day1/src/main.rs
@@ -6,7 +6,40 @@ use atty::Stream;
 const DEFAULT_FILENAME: &str = "day1.txt";
 
 fn parse_calibration_value(s: &str) -> u32 {
-    let digits: Vec<&str> = s.matches(|c: char| c.is_ascii_digit()).collect();
+    let digits: Vec<char> =  s.char_indices().filter_map(|(_, c)| match "0123456789".find(c) {
+        Some(_) => Some(c),
+        None => None,
+    }).collect();
+
+    if digits.is_empty() {
+        0
+    } else {
+        let first_digit = digits.first().unwrap();
+        let last_digit = digits.last().unwrap();
+
+        format!("{first_digit}{last_digit}").parse().unwrap()
+    }
+}
+
+fn parse_correct_calibration_value(s: &str) -> u32 {
+    let digits: Vec<char> = s.char_indices().filter_map(|(i, c)| {
+        if let Some(_) = "0123456789".find(c) {
+            Some(c)
+        } else {
+            match c {
+                'o' if s[i..].starts_with("one") => Some('1'),
+                't' if s[i..].starts_with("two") => Some('2'),
+                't' if s[i..].starts_with("three") => Some('3'),
+                'f' if s[i..].starts_with("four") => Some('4'),
+                'f' if s[i..].starts_with("five") => Some('5'),
+                's' if s[i..].starts_with("six") => Some('6'),
+                's' if s[i..].starts_with("seven") => Some('7'),
+                'e' if s[i..].starts_with("eight") => Some('8'),
+                'n' if s[i..].starts_with("nine") => Some('9'),
+                _ => None
+            }
+        }
+    }).collect();
 
     let first_digit = digits.first().unwrap();
     let last_digit = digits.last().unwrap();
@@ -22,12 +55,17 @@ fn main() {
         Box::new(BufReader::new(fs::File::open(filename).unwrap()))
     };
 
-    let mut result: u32 = 0;
+    let mut part1: u32 = 0;
+    let mut part2: u32 = 0;
     for line in reader.lines() {
         if let Ok(l) = line {
-            result += parse_calibration_value(&l);
+            if !l.is_empty() {
+                part1 += parse_calibration_value(&l);
+                part2 += parse_correct_calibration_value(&l);
+            }
         }
     }
 
-    println!("{result}");
+    println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
 }
