d8deb058fdccde9ce7544e0e192872838642f22b
Roland Tepp <roland@gennet.ee>
2023-12-10 13:50:19 +0200

    Day 3, part 2, wrong solution
    This is refactoring of the solution to Day 3, part 1 to solve part 2, but it seems to be still wrong.
    Committing this solution to be able to come back to it at some later date.
    Hopefully...

--- crates/day3/src/main.rs
+++ crates/day3/src/main.rs
@@ -1,10 +1,30 @@
-use std::{env, fs, io};
+use std::cmp::min;
 use std::io::{BufRead, BufReader};
+use std::ops::RangeInclusive;
+use std::{env, fs, io};
 
+use crate::Char::{Blank, Digit, Symbol};
 use anyhow::{bail, Context, Result};
 use atty::Stream;
+use itertools::izip;
 
 const DEFAULT_FILENAME: &str = "day3.txt";
+
+enum Char {
+    Digit(char),
+    Symbol(char),
+    Blank,
+}
+
+impl From<char> for Char {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Blank,
+            _ if value.is_ascii_digit() => Digit(value),
+            _ => Symbol(value),
+        }
+    }
+}
 
 fn main() -> Result<()> {
     let filename = env::args()
@@ -21,79 +41,233 @@ fn main() -> Result<()> {
     };
 
     let mut original_lines: Vec<String> = reader.lines().filter_map(|l| l.ok()).collect();
-    let original_lines_count = original_lines.len();
 
-    let line_length = original_lines.first().map(|s| s.chars().count()).unwrap_or_default();
+    let line_length = original_lines
+        .first()
+        .map(|s| s.chars().count())
+        .unwrap_or_default();
     let blank_line = ".".repeat(line_length);
 
-    let mut lines: Vec<String> = Vec::with_capacity(original_lines_count + 2);
+    let mut lines: Vec<String> = Vec::with_capacity(original_lines.len() + 2);
     lines.push(blank_line.to_string());
     lines.append(&mut original_lines);
     lines.push(blank_line.to_string());
 
     let mut part1 = 0u32;
+    let mut part2 = 0u32;
     let lines = lines.as_slice();
-    for slice in lines.windows(3) {
-        let [previous, current, next] = slice else { bail!("Expected window of three lines") };
-        let mut previous = previous.as_str();
-        let mut current = current.as_str();
-        let mut next = next.as_str();
+    for window in lines.windows(3) {
+        let [previous, current, next] = window else {
+            bail!("Expected window of three lines")
+        };
 
-        let mut symbol: bool = false;
+        eprintln!("\n{}", "=".repeat(line_length + 2));
+        if !current.contains(|c: char| !c.is_ascii_digit() && c != '.') {
+            eprintln!("Skipping:\n {} \n→{}←\n {} ", previous, current, next);
+            continue;
+        }
 
-        while !current.is_empty() {
-            let number = if current.starts_with(|c: char| c.is_ascii_digit()) {
-                let n: String = current.chars().take_while(|c| c.is_ascii_digit()).collect();
-                Some(n)
+        let slice = izip!(
+            previous.char_indices(),
+            current.char_indices(),
+            next.char_indices()
+        );
+        let sl = slice.map(|((i, a), (_, b), (_, c))| (i, [a, b, c]));
+
+        let mut start: [Option<usize>; 3] = [None; 3];
+        let mut symbol: Option<usize> = None;
+        let mut gear: Option<usize> = None;
+
+        let mut numbers: Vec<u32> = Vec::with_capacity(6);
+
+        for (i, ccc) in sl {
+            eprintln!("{} ↓", " ".repeat(i));
+            let i0 = start[0].unwrap_or(i);
+            eprintln!(
+                "{}[{}]{}",
+                previous.get(..i0).unwrap_or_default(),
+                previous.get(i0..(i + 1)).unwrap_or_default(),
+                previous.get((i + 1)..).unwrap_or_default()
+            );
+            let i0 = start[1].unwrap_or(i);
+            eprintln!(
+                "{}[{}]{}",
+                current.get(..i0).unwrap_or_default(),
+                current.get(i0..(i + 1)).unwrap_or_default(),
+                current.get((i + 1)..).unwrap_or_default()
+            );
+            let i0 = start[2].unwrap_or(i);
+            eprintln!(
+                "{}[{}]{}",
+                next.get(..i0).unwrap_or_default(),
+                next.get(i0..(i + 1)).unwrap_or_default(),
+                next.get((i + 1)..).unwrap_or_default()
+            );
+            eprintln!("{} ↑", " ".repeat(i));
+
+            let mut res: [(Option<u32>, bool, bool); 3] = Default::default();
+            for n in 0..3 {
+                let (number, s, g) = match Char::from(ccc[n]) {
+                    Digit(_) => {
+                        if start[n].is_none() {
+                            start[n] = Some(i);
+                        }
+                        if i + 1 == line_length {
+                            let num = parse_number(&window[n], start[n], i + 1);
+                            (num, false, false)
+                        } else {
+                            (None, false, false)
+                        }
+                    }
+                    Symbol(c) => {
+                        let num = parse_number(&window[n], start[n], i);
+                        (num, true, c == '*')
+                    }
+                    Blank => {
+                        let num = parse_number(&window[n], start[n], i);
+                        (num, false, false)
+                    }
+                };
+
+                eprint!("{} number: {number:?}", ["⎧", "⎬", "⎩"][n]);
+                if s {
+                    eprint!("; symbol");
+                }
+                if g {
+                    eprint!("; gear");
+                }
+                eprintln!();
+
+                res[n] = (number, s, g);
+            }
+
+            let (_, symbol1, gear1) = res[1];
+            if symbol1 {
+                symbol = Some(i);
+            }
+
+            if gear1 {
+                gear = Some(i);
+            }
+
+            let numbers_with_start = izip!(res.map(|(num, _, _)| num), start)
+                .filter_map(|(num, start)| num.and_then(|num| start.map(|start| (start, num))))
+                .collect::<Vec<(usize, u32)>>();
+
+            if let Some(symbol) = symbol {
+                for (start, num) in numbers_with_start.iter() {
+                    eprintln!("❯❯ start: {start}; symbol: {symbol}, number: {num}");
+                    if *start <= symbol || start.abs_diff(symbol) < 2 {
+                        part1 += *num;
+                    }
+                }
+            }
+
+            if let Some(gear) = gear {
+                for (start, num) in numbers_with_start.iter() {
+                    eprintln!("❯❯ start: {start}; gear: {gear}, number: {num}");
+                    if *start <= gear || start.abs_diff(gear) < 2 {
+                        numbers.push(*num);
+                    }
+                }
+            }
+
+            eprintln!("❯ symbol: {symbol:?}; gear: {gear:?}, numbers: {numbers:?}");
+            eprintln!("❯ part1 = {part1}");
+            eprintln!("❯ part2 = {part2}");
+            if ['.', '.', '.'] == ccc || i + 1 == line_length {
+                if !numbers.is_empty() {
+                    if numbers.len() > 1 {
+                        let gear_ratio: u32 = numbers.iter().product();
+                        eprint!("❯ part2: {part2} + {}", gear_ratio);
+                        part2 += gear_ratio;
+                        eprintln!(" = {part2}");
+                    }
+                    numbers.clear();
+                }
+            }
+
+            for n in 0..3 {
+                if let Some(_) = res[n].0 {
+                    start[n] = None
+                }
+            }
+
+            eprintln!("\n{}\n", "~".repeat(line_length + 2));
+        }
+        eprintln!("{}\n", "=".repeat(line_length + 2));
+    }
+
+    println!("Day3, Part1: {part1}");
+    println!("Day3, Part2: {part2}");
+    Ok(())
+}
+
+fn parse_number(line: &String, start: Option<usize>, end: usize) -> Option<u32> {
+    let num = start
+        .and_then(|it| line.get(it..end))
+        .and_then(|s| s.parse::<u32>().ok());
+    num
+}
+
+fn parse_slice(s: &str, range: RangeInclusive<usize>) -> Option<u128> {
+    let slice: &str = s.get(range)?;
+    slice.parse().ok()
+}
+
+fn find_nearby_numbers(gears: &Vec<usize>, line: &str) -> Vec<u128> {
+    if gears.is_empty() {
+        return Vec::new();
+    }
+
+    let mut gears = gears.iter();
+    let Some(first_gear) = gears.next() else {
+        return Vec::new();
+    };
+
+    let first_start = if *first_gear > 0 {
+        *first_gear - 1
+    } else {
+        *first_gear
+    };
+    let first_range = first_start..min(line.len(), first_start + 3);
+    println!("{}", line.get(first_range).unwrap_or_default());
+
+    let mut numbers: Vec<u128> = Vec::with_capacity(2 * gears.len());
+
+    let mut gear = first_gear;
+    let mut range: Option<RangeInclusive<usize>> = None;
+
+    for (i, c) in line.char_indices() {
+        if c.is_ascii_digit() {
+            if let Some(r) = &range {
+                range = Some(*r.start()..=i);
             } else {
-                None
-            };
-
-            if let Some(n) = &number {
-                let (a_prefix, a_rest) = previous.split_at(n.len());
-                previous = a_rest;
-
-                (_, current) = current.split_at(n.len());
-
-                let (c_prefix, c_rest) = next.split_at(n.len());
-                next = c_rest;
-
-                symbol |= [a_prefix, c_prefix].join("")
-                    .contains(|c: char| !(c.is_ascii_digit() || c == '.'));
-
-                if symbol {
-                    eprintln!("Found a part number: {n}");
-                    part1 += n.parse::<u32>()?;
-                    symbol = a_prefix.ends_with(|c: char| !(c.is_ascii_digit() || c == '.'))
-                          || c_prefix.ends_with(|c: char| !(c.is_ascii_digit() || c == '.'));
-                }
+                range = Some(i..=i);
             }
+            continue;
+        }
 
-            if current.is_empty() {
+        if !c.is_ascii_digit() {
+            if let Some(r) = &range {
+                let (start, end) = r.clone().into_inner();
+                let start_bound = if start > 0 { start - 1 } else { start };
+                if start_bound <= *gear && *gear <= end + 1 {
+                    if let Some(number) = parse_slice(line, start..=end) {
+                        numbers.push(number);
+                    }
+                }
+                range = None;
+            }
+        }
+
+        if i > *gear && i.abs_diff(*gear) > 1 {
+            let Some(next_gear) = gears.next() else {
                 break;
-            }
-
-            let (a, a_rest) = previous.split_at(1);
-            let (b, b_rest) = current.split_at(1);
-            let (c, c_rest) = next.split_at(1);
-
-            previous = a_rest;
-            current = b_rest;
-            next = c_rest;
-
-            symbol = [a, b, c].join("")
-                .contains(|c: char| !(c.is_ascii_digit() || c == '.'));
-
-            if let Some(n) = &number {
-                if symbol {
-                    //eprintln!("Found a part number: {n}");
-                    part1 += n.parse::<u32>()?;
-                }
-            }
+            };
+            gear = next_gear;
         }
     }
 
-
-    println!("Day3, Part1: {part1}");
-    Ok(())
+    numbers
 }
